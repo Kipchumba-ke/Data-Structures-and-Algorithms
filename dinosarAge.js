2becac96e1ba7747a49f8109b1dfed17c280244e
Kipchumba <chogekelvin90@gmail.com>
2026-09-09 18:14:19 +0300

    Add function to find first unique dinosaur age
    Implement function to find the first unique dinosaur age from an array of ages using a queue and a frequency map.

--- dinosarAge.js
+++ dinosarAge.js
@@ -1,1 +1,22 @@
+import { Queue } from './'
 
+export function findFirstUniqueDinosaurAge(ages) {
+  const queue = new Queue()
+  const ageFrequency = new Map()
+  for(const age of ages){
+    queue.enqueue(age)
+    if(!ageFrequency.has(age)){
+      ageFrequency.set(age, 1)
+    }else {
+      ageFrequency.set(age, ageFrequency.get(age) + 1)
+    }
+  }
+  while(!queue.isEmpty()){
+    const age = queue.dequeue()
+    if(ageFrequency.get(age) === 1){
+      return age
+    }
+  }
+  
+  return "No unique age was found."
+}
